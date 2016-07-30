1351f7b63ef62e8ef18c97371c2a4ecf6119df26
Pablo Venegas <pvenegas@odecee.com.au>
2016-07-30 10:08:40 +1000

    echo when processing shell files

--- 01-centos7-docker/bootstrap.sh
+++ 01-centos7-docker/bootstrap.sh
@@ -2,6 +2,8 @@
 
 # https://docs.docker.com/engine/installation/linux/centos/
 
+echo "** Processing install-docker.sh"
+
 sudo yum update -y
 curl -fsSL https://get.docker.com/ | sh
 sudo service docker start
@@ -12,3 +14,5 @@ sudo usermod -aG docker vagrant
 sudo chkconfig docker on
 
 # docker run hello-world
+
+echo "** Processing install-docker.sh - Complete"
--- 01-centos7-docker/install-dockercompose.sh
+++ 01-centos7-docker/install-dockercompose.sh
@@ -2,5 +2,9 @@
 
 # https://docs.docker.com/compose/install/
 
+echo "** Processing install-dockercompose.sh"
+
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "** Processing install-dockercompose.sh - Complete"
